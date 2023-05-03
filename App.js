e2fa9e4ccd181522cf32faded979ffad5f650a46
fabianlopes <60930772+fabianlopes@users.noreply.github.com>
2023-05-03 16:22:18 -0400

    primeiro

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { FlatList, StyleSheet, Text, View, TextInput } from 'react-native';
+import { useState } from 'react';
+
+
+export default function App() {
+  let listaInicial = ['Caderno', 'Lapis'];
+  let [listaMateriais, definirListaMateriais] = useState(listaInicial);
+
+  return (
+    <View style={styles.container}>
+      <Text>APP - Lista de Material Escolar</Text>
+        <FlatList
+          data = {listaMateriais}
+          renderItem={({item}) => <Text>{item}</Text>}
+        />
+        <TextInput
+          placeholder={'Inserir Novo Item'}
+          onSubmitEditing={({nativeEvent})=>
+          definirListaMateriais(listaMateriais.concat(nativeEvent.text))
+          }
+        />
+
+      </View>
+  );
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
